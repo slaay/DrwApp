85d74750d58345bdc1514bc19a1abf9e23285cde
PresleyDias <presley.diaslee@gmail.com>
2015-02-11 23:34:45 +0530

    Xibs removed and Storyboard added
    Xibs removed and Storyboard added

--- DrwApp/DAScratchPadExample/DAAppDelegate.m
+++ DrwApp/DAScratchPadExample/DAAppDelegate.m
@@ -14,15 +14,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-	    self.viewController = [[DAViewController alloc] initWithNibName:@"DAViewController_iPhone" bundle:nil];
-	} else {
-	    self.viewController = [[DAViewController alloc] initWithNibName:@"DAViewController_iPad" bundle:nil];
-	}
-	self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
+    // Override point for customization after application launch.
+    
+    // Change the background color of navigation bar
+    [[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
+    
+    // Change the font style of the navigation bar
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 0);
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:10.0/255.0 green:10.0/255.0 blue:10.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           shadow, NSShadowAttributeName,
+                                                           [UIFont fontWithName:@"Helvetica-Light" size:21.0], NSFontAttributeName, nil]];
+    
     return YES;
 }
 
