87ae4530ef20a0fa2be87e5f98462450a2fa4902
PresleyDias <presley.diaslee@gmail.com>
2015-02-26 22:37:38 +0530

    Code added to show the tumblerMenu
    Code added to show the tumblerMenu and images added

---
+++ DrwApp/DAScratchPadExample/controllers/AboutAppViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutAppViewController.h
+//  DrwApp
+//
+//  Created by Presley on 26/02/15.
+//  Copyright (c) 2015 Double Apps Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutAppViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *btnImg;
+
+@end
---
+++ DrwApp/DAScratchPadExample/controllers/AboutAppViewController.m
@@ -0,0 +1,90 @@
+//
+//  AboutAppViewController.m
+//  DrwApp
+//
+//  Created by Presley on 26/02/15.
+//  Copyright (c) 2015 Double Apps Inc. All rights reserved.
+//
+
+#import "AboutAppViewController.h"
+#import "CHTumblrMenuView.h"
+
+@interface AboutAppViewController ()
+
+@end
+
+@implementation AboutAppViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self AddTumblerStyleMenuControls:_btnImg];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(void)AddTumblerStyleMenuControls:(id)sender{
+     UIButton* btnImg = (UIButton*)sender;
+    [btnImg addTarget:self action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+
+}
+
+- (void)showMenu
+{
+    CHTumblrMenuView *menuView = [[CHTumblrMenuView alloc] init];
+    [menuView addMenuItemWithTitle:@"Text" andIcon:[UIImage imageNamed:@"post_type_bubble_text.png"] andSelectedBlock:^{
+        NSLog(@"Text selected");
+    }];
+    [menuView addMenuItemWithTitle:@"Photo" andIcon:[UIImage imageNamed:@"post_type_bubble_photo.png"] andSelectedBlock:^{
+        NSLog(@"Photo selected");
+    }];
+    [menuView addMenuItemWithTitle:@"Quote" andIcon:[UIImage imageNamed:@"post_type_bubble_quote.png"] andSelectedBlock:^{
+        NSLog(@"Quote selected");
+        
+    }];
+    [menuView addMenuItemWithTitle:@"Link" andIcon:[UIImage imageNamed:@"post_type_bubble_link.png"] andSelectedBlock:^{
+        NSLog(@"Link selected");
+        
+    }];
+    [menuView addMenuItemWithTitle:@"Chat" andIcon:[UIImage imageNamed:@"post_type_bubble_chat.png"] andSelectedBlock:^{
+        NSLog(@"Chat selected");
+        
+    }];
+    [menuView addMenuItemWithTitle:@"Video" andIcon:[UIImage imageNamed:@"post_type_bubble_video.png"] andSelectedBlock:^{
+        NSLog(@"Video selected");
+        
+    }];
+    
+    
+    
+    [menuView show];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
